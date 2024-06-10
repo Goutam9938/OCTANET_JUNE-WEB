64db3f345ecd564f22abc9ac73bd0f823b7d1b26
Goutam Nayak <Gounayak1@gmail.com>
2024-06-10 18:08:32 +0530

    Task 1 Commit

---
+++ Task - 1/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.signup-form form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for signing up! We will contact you soon.');
+});
